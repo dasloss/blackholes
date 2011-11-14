ee84e6a74afae46185828ad32e746f17df162e10
Kit La T <kit.la.t@gmail.com>
2011-11-13 18:28:16 -0700

    Added custom error handling.

--- app/views.py
+++ app/views.py
@@ -9,3 +9,14 @@ views = Blueprint('views', __name__, static_folder='../static',
 def index():
     """Render index."""
     return render_template('index.html')
+
+@views.app_errorhandler(404)
+def page_not_found(error):
+    """Custom 404 page."""
+    return render_template('404.html'), 404
+
+@views.app_errorhandler(500)
+def page_not_found(error):
+    """Custom 500 page."""
+    # Insert error logging here.
+    return render_template('500.html'), 500
