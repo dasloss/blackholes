5d1bbac2900c7ec867545840cf21f8d1a7269610
Kit La T <kit.la.t@gmail.com>
2011-11-15 16:24:13 -0700

    Added the skeleton of a unit test file.

---
+++ app/test.py
@@ -0,0 +1,11 @@
+import unittest
+import app
+
+class AppTestCase(unittest.TestCase):
+    def setUp(self):
+        pass
+    def tearDown(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
