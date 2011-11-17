9f9c0ea7e2f4000d678ccf4412ce93143d09c14e
Kit La T <kit.la.t@gmail.com>
2011-11-16 23:49:44 -0700

    Made OAuth logins persistent.

--- app/views.py
+++ app/views.py
@@ -10,19 +10,10 @@ login_manager = LoginManager()
 
 oauth = OAuth()
 twitter = oauth.remote_app('twitter',
-    # unless absolute urls are used to make requests, this will be added
-    # before all URLs.  This is also true for request_token_url and others.
     base_url='http://api.twitter.com/1/',
-    # where flask should look for new request tokens
     request_token_url='http://api.twitter.com/oauth/request_token',
-    # where flask should exchange the token with the remote application
     access_token_url='http://api.twitter.com/oauth/access_token',
-    # twitter knows two authorizatiom URLs.  /authorize and /authenticate.
-    # they mostly work the same, but for sign on /authenticate is
-    # expected because this will give the user a slightly different
-    # user interface on the twitter side.
     authorize_url='http://api.twitter.com/oauth/authenticate',
-    # the consumer keys from the twitter application registry.
     consumer_key='xBeXxg9lyElUgwZT6AZ0A',
     consumer_secret='aawnSpNTOVuDCjx7HMh6uSXetjNN8zWLpZwCEU4LBrk'
 )
@@ -62,7 +53,7 @@ def oauth_authorized(resp):
     user.oauth_secret = resp['oauth_token_secret']
     user.authenticated = True
     user.save()
-    login_user(user)
+    login_user(user, remember=True)
     return redirect(next_url)
 
 @views.route('/register/', methods=['GET', 'POST'])
